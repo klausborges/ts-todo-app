0380447dac045f7ab84a189b324a4b8d5e7abd93
Klaus Borges <klausborgs@gmail.com>
2020-10-26 15:27:24 -0300

    feat: add basic functionality to create & list tasks

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-import { Container, Form } from './styles';
+import { Container, Form, TodoList, TodoHeader, TodoItem } from './styles';
 
 interface TodoItem {
   id: string;
@@ -12,13 +12,53 @@ interface TodoItem {
 
 const Todo: React.FC = () => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      if (!newTodo) return;
+
+      setTodos([
+        ...todos,
+        {
+          id: uuidv4(),
+          description: newTodo,
+          createdAt: new Date(),
+          completedAt: null,
+        },
+      ]);
+
+      setNewTodo('');
+    },
+    [todos, newTodo],
+  );
 
   return (
     <Container>
-      <Form>
-        <input />
+      <Form onSubmit={handleAddTodo}>
+        <input
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+          placeholder="What do you need to do?"
+        />
         <button type="submit">+</button>
       </Form>
+
+      <TodoList>
+        <TodoHeader>
+          <strong>Tasks</strong>
+        </TodoHeader>
+
+        {todos.map(todo => (
+          <TodoItem key={todo.id}>
+            <input type="checkbox" checked={todo.completedAt !== null} />
+            <span>{todo.description}</span>
+            <button type="button">X</button>
+          </TodoItem>
+        ))}
+      </TodoList>
     </Container>
   );
 };
--- src/pages/Todo/styles.ts
+++ src/pages/Todo/styles.ts
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.main``;
 
 export const Form = styled.form``;
+
+export const TodoList = styled.section``;
+
+export const TodoHeader = styled.header``;
+
+export const TodoItem = styled.div``;
